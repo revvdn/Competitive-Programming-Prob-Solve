d507b3664db81ec6dd6784abd55cfdb7d972a02e
revvdn <revanadnanperdana@gmail.com>
2026-05-05 21:49:42 +0700

    adding indonesian NOI prob solve

---
+++ problems/Indonesian_NOI/2025/day_1_A.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<tuple>
+#include<algorithm>
+#define int long long 
+
+const int MAXN = 2e5+5;
+const int INF = 1e18;
+
+std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> dijkstra;
+std::vector<std::tuple<int,int,int>> adj[MAXN];
+std::vector<std::pair<int,int>> rain;
+std::vector<std::pair<int,int>> res;
+std::vector<int> que;
+int ceil[MAXN]; 
+int dist[MAXN];
+
+void filling () {
+    for (int i=0; i<MAXN; i++) {
+        dist[i] = ceil[i] = INF;
+    }
+}
+
+void input (int u, int v, int w, int z) {
+    adj[u].push_back({v,w,z});
+    adj[v].push_back({u,w,z});
+}
+
+void dijkstra_ceil (int n) {
+    dijkstra.push({0,n});
+    while (!dijkstra.empty()) {
+        auto [d, u] = dijkstra.top();
+        dijkstra.pop();
+        
+        if (ceil[u] <= d) continue;
+        ceil[u] = d;
+        
+        for (auto [nxt, w, z] : adj[u]) {
+            if (z == 1) continue;
+
+            if (ceil[nxt] <= ceil[u] + w) continue;
+            dijkstra.push({ceil[u] + w, nxt});
+        }
+    }
+}
+
+void dijkstra_dist () {
+    dijkstra.push({0, 1});
+    while (!dijkstra.empty()) {
+        auto [d, u] = dijkstra.top();
+        dijkstra.pop();
+
+        if (dist[u] <= d) continue;
+        dist[u] = d;
+
+        if (ceil[u] != INF) {
+            res.push_back({dist[u], dist[u] + ceil[u]});
+        }
+
+        for (auto [nxt, w, z] : adj[u]) {
+            if (z == 2) continue;
+
+            if (dist[nxt] <= dist[u] + w) continue;
+            dijkstra.push({dist[u] + w, nxt});
+        }
+    }
+}
+
+void solve () {
+    int n,m; std::cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        int u,v,w,z;
+        std::cin >> u >> v >> w >> z;
+        input(u,v,w,z);
+        if (z == 2) input(u,v,w,1);
+    }
+
+    int q; std::cin >> q;
+    for (int i = 1; i <= q; i++) {
+        int x; std::cin >> x;
+        que.push_back(x);
+    }
+
+    dijkstra_ceil(n);
+    while (!dijkstra.empty()) dijkstra.pop();
+    dijkstra_dist();
+
+    std::sort(res.begin(), res.end());
+
+    int idx = INF;
+    for (int i = 0; i < res.size(); i++) {
+        idx = std::min(res[i].second, idx);
+        res[i].second = idx;
+    }
+
+    for (auto i : que) {
+        int l = 0, r = res.size() - 1;
+        int ans = -1;
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (res[m].first > i) {
+                r = m - 1;
+            }
+            else {
+                ans = res[m].second;
+                l = m + 1;
+            }
+        }
+        std::cout << ans << "\n";    
+    }
+}
+
+signed main () {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    filling();
+
+    int t; t = 1;
+    while (t--) solve();
+    return 0;
+}
