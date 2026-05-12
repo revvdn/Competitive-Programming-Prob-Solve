60a0794b98e8201a98767af1df3f45392f8dfb33
revvdn <revanadnanperdana@gmail.com>
2026-05-12 14:39:45 +0700

    adding 2025 OSP A

--- problems/TLX/Indonesian_NOI_Preliminary/2025_A.cpp
+++ problems/TLX/Indonesian_NOI_Preliminary/2025_A.cpp
@@ -1,32 +1,29 @@
-#include <iostream>
-#include <cstring>
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+const int INF = 1e8;
 
 bool check (char c) {
     if (c == 'O' || c == 'S' || c == 'N') return true;
+    return false;
 }
 
 int main () {
     std::string s; std::cin >> s;
     int n = s.size();
     int memo[n + 1][2];
-    //memo[i][0] buat ngitung kemunculan O | S | N
-    //memo[i][1] buat ngitung kemunculan P
-    memset(memo, sizeof(memo), 0);
-    if (check(s[0])) memo[0][0] = 1;
-    if (s[0] == 'P') memo[0][1] = 1;
+    memset(memo, INF, sizeof(memo));
+    memo[0][0] = memo[0][1] = 0;
     for (int i = 1; i < n; i++) {
-        if (check(s[i])) {
-            memo[i][0] = memo[i-1][0] + 1; 
-        }
-        if (s[i] == 'P') {
-            memo[i][1] = memo[i-1][1] + 1;
-        }
+        memo[i][0] = memo[i-1][0] + (check(s[i]) ? 1 : 0);
+        memo[i][1] = memo[i-1][1] + (s[i] == 'P' ? 1: 0); 
     }
+
     int ans = -1;
-    for (int i = 0; i < n-1; i++) {
-        int tmp = memo[i][0] + memo[n][1] - memo[i+1][0];
-        ans = (ans < tmp ? tmp : ans);
+    for (int i = 0; i < n; i++) {
+        int tmp = memo[i][0] + memo[n][1] - memo[i][1];
+        ans = std::max(ans, tmp); 
     }
     std::cout << ans;
-    return 0;
 }
